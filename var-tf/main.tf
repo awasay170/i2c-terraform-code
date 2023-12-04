ad283e8d61f33849b1ccfc501c47526518854083
abdul wasay <awasay@i2cinc.com>
2023-12-04 20:35:26 +0000

    var terraform'

---
+++ var-tf/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "3.0.2"
+     }
+  } 
+
+}
+provider "docker" {
+}
+
+resource "docker_image" "nginx" {
+  name = "nginx:1.19.6"
+  keep_locally = true
+}
+
+resource "docker_container" "new_container" {
+  name = var.container_name
+  image = docker_image.nginx.image_id
+  ports {
+    internal = 80
+    external = 2224
+ }
+}
+
